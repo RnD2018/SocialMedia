32a1a3d7c99ae75cf0a67a07fa072afe77a8a5bc
Abhijith Chandra <abhijithchndr8@gmail.com>
2018-06-03 22:45:42 +0530

    Added messaging
    Added few functionalities to send messages to server and retrieve all the messages related to the user.

--- SocialMedia/client.cpp
+++ SocialMedia/client.cpp
@@ -3,71 +3,105 @@
 
 #include<stdio.h>
 #include<winsock2.h>
+#include "messaging.h"
 
 #pragma comment(lib,"ws2_32.lib") //Winsock Library
 
-int main(int argc, char *argv[])
+
+
+//
+//int main(int argc, char *argv[])
+//{
+//	WSADATA wsa;
+//	SOCKET s;
+//	struct sockaddr_in server;
+//	char *message, server_reply[5000];
+//	int recv_size;
+//
+//	printf("\nInitialising Winsock...");
+//	if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0)
+//	{
+//		printf("Failed. Error Code : %d", WSAGetLastError());
+//		return 1;
+//	}
+//
+//	printf("Initialised.\n");
+//
+//	//Create a socket
+//	if ((s = socket(AF_INET, SOCK_STREAM, 0)) == INVALID_SOCKET)
+//	{
+//		printf("Could not create socket : %d", WSAGetLastError());
+//	}
+//
+//	printf("Socket created.\n");
+//
+//
+//	//server.sin_addr.s_addr = inet_addr("192.168.137.1");
+//	server.sin_addr.s_addr = inet_addr("192.168.43.80");
+//	server.sin_family = AF_INET;
+//	server.sin_port = htons(8888);
+//
+//	//Connect to remote server
+//	if (connect(s, (struct sockaddr *)&server, sizeof(server)) < 0)
+//	{
+//		puts("connect error");
+//		return 1;
+//	}
+//
+//	char buffer[100];
+//	puts("Connected");
+//	while (1){
+//		printf("\nEnter message: ");
+//		scanf("%s", buffer);
+//		send(s, buffer, strlen(buffer), 0);
+//		//Receive a reply from the server
+//		if ((recv_size = recv(s, server_reply, 2000, 0)) > 0)
+//		{
+//			server_reply[recv_size] = '\0';
+//			printf("%s\n", server_reply);
+//		}
+//	}
+//
+//	puts("Reply received\n");
+//
+//	//Add a NULL terminating character to make it a proper string before printing
+//	server_reply[recv_size] = '\0';
+//	puts(server_reply);
+//
+//
+//	closesocket(s);
+//	WSACleanup();
+//
+//	return 0;
+//}
+
+struct x{
+	char user[20];
+};
+int main()
 {
-	WSADATA wsa;
-	SOCKET s;
-	struct sockaddr_in server;
-	char *message, server_reply[5000];
-	int recv_size;
+	/*FILE* fp = fopen("test.txt", "w");
+	fwrite("hai", 4, 1, fp);
+	fclose(fp);
+	printf("Written!\n");
+	system("pause");*//*
+	struct x obj;
+	strcpy(obj.user, "hai");
 
-	printf("\nInitialising Winsock...");
-	if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0)
+	printf("%s", obj.user);*/
+
+	/*SOCKET s=NULL;
+	send_message("abhijith", s);*/
+
+	FILE* fp = fopen("test.txt", "rb+");
+	if (fp == NULL)
 	{
-		printf("Failed. Error Code : %d", WSAGetLastError());
-		return 1;
+		fp = fopen("test.txt", "wb");
+		fclose(fp);
+		fp = fopen("test.txt", "rb+");
 	}
-
-	printf("Initialised.\n");
-
-	//Create a socket
-	if ((s = socket(AF_INET, SOCK_STREAM, 0)) == INVALID_SOCKET)
-	{
-		printf("Could not create socket : %d", WSAGetLastError());
-	}
-
-	printf("Socket created.\n");
-
-
-	//server.sin_addr.s_addr = inet_addr("192.168.137.1");
-	server.sin_addr.s_addr = inet_addr("192.168.43.80");
-	server.sin_family = AF_INET;
-	server.sin_port = htons(8888);
-
-	//Connect to remote server
-	if (connect(s, (struct sockaddr *)&server, sizeof(server)) < 0)
-	{
-	
-		puts("connect error");
-		return 1;
-	}
-
-	char buffer[100];
-	puts("Connected");
-	while (1){
-		printf("\nEnter message: ");
-		scanf("%s", buffer);
-		send(s, buffer, strlen(buffer), 0);
-		//Receive a reply from the server
-		if ((recv_size = recv(s, server_reply, 2000, 0)) > 0)
-		{
-			server_reply[recv_size] = '\0';
-			printf("%s\n", server_reply);
-		}
-	}
-
-	puts("Reply received\n");
-
-	//Add a NULL terminating character to make it a proper string before printing
-	server_reply[recv_size] = '\0';
-	puts(server_reply);
-
-
-	closesocket(s);
-	WSACleanup();
-
+	fseek(fp, 1024 * 512 + 148480, SEEK_SET);
+	fwrite("1", 1, 1, fp);
+	fclose(fp);
 	return 0;
 }
